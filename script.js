d1705aaa70f0a9db5de8fd31527946565dd544ea
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-03-11 14:49:22 +0300

    added jquery effect to make what we do div interactive

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+$(".what2").click(function(){
+  $("#hide2").slideToggle()
+  $("#show2").slideToggle()
+})
+
+$(".what3").click(function(){
+    $("#hide3").slideToggle()
+    $("#show3").slideToggle()
+  })
+
+  $(".what4").click(function(){
+    $("#hide4").slideToggle()
+    $("#show4").slideToggle()
+  })
+
+})
